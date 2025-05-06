5387ffc3f751d766d0936c842d0f2aa5320c542f
rishadpmohammad <rishadp@vofoxsolutions.com>
2025-05-06 14:29:40 +0530

    buttontitle

--- CrewBid iPad_Swift/Views/ViewControllers/Brightness/CBBrightnessViewController.swift
+++ CrewBid iPad_Swift/Views/ViewControllers/Brightness/CBBrightnessViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class CBBrightnessViewController: UIViewController {
     @IBOutlet weak var brightnessSlider: UISlider!
     @IBOutlet weak var darkModeSwitch: UISwitch!
+    @IBOutlet weak var btnBack: UIButton!
     
 
     override func viewDidLoad() {
@@ -17,15 +18,10 @@ class CBBrightnessViewController: UIViewController {
         brightnessSlider.value = Float(UIScreen.main.brightness)
         brightnessSlider.minimumValue = 0.0
         brightnessSlider.maximumValue = 1.0
+        btnBack.setTitle("", for: .normal)
 
         if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
            let window = windowScene.windows.first {
-            
-//            // Reset to system default mode unless user sets it explicitly
-//            if window.overrideUserInterfaceStyle != .unspecified {
-//                window.overrideUserInterfaceStyle = .unspecified
-//            }
-            
             // Reflect the current system appearance in the switch
             darkModeSwitch.isOn = (window.traitCollection.userInterfaceStyle == .dark)
         }
