07aafbdf9faba7381a84c330f606c2e57ce17097
rishad <rishadp@vofoxsolutions.com>
2025-05-14 15:53:41 +0530

    added bid delete

--- CrewBid iPad_Swift/Views/Cells/HomescreenDocCell/DocumentCell.swift
+++ CrewBid iPad_Swift/Views/Cells/HomescreenDocCell/DocumentCell.swift
@@ -16,4 +16,53 @@ class DocumentCell: UICollectionViewCell {
     @IBOutlet weak var position: UILabel!
     @IBOutlet weak var userIconView: UIImageView!
     
+    //MARK: - Wiggle Animation
+    private let kWiggleAnimationKey = "wiggle"
+    // Function to start a wiggling animation on a view.
+    
+    func startWiggleAnimation() {
+        let kWiggleAnimationAngle: CGFloat = 0.01
+        // Define the starting and ending transformation for the wiggle animation.
+        
+        let startTransform = CATransform3DMakeRotation(-kWiggleAnimationAngle, 0, 0, 1.0)
+        let endTransform = CATransform3DMakeRotation(kWiggleAnimationAngle, 0, 0, 1.0)
+        let animation = CAKeyframeAnimation(keyPath: "transform")
+        animation.values = [NSValue(caTransform3D: startTransform), NSValue(caTransform3D: endTransform)]
+        animation.repeatCount = 100
+        animation.duration = 0.125
+        animation.autoreverses = true
+        layer.transform = startTransform
+        layer.add(animation, forKey: kWiggleAnimationKey)
+    }
+    // Function to stop the wiggling animation on a view.
+    
+    func stopWiggleAnimation() {
+        layer.removeAnimation(forKey: kWiggleAnimationKey)
+        layer.transform = CATransform3DMakeRotation(0.0, 0, 0, 1.0)
+    }
+    // Function to make a view shake.
+    
+    func shake() {
+        let shakeAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        shakeAnimation.duration = 0.05
+        shakeAnimation.repeatCount = 2
+        shakeAnimation.autoreverses = true
+        let startAngle: Float = (-2) * 3.14159/180
+        let stopAngle = -startAngle
+        shakeAnimation.fromValue = NSNumber(value: startAngle as Float)
+        shakeAnimation.toValue = NSNumber(value: 3 * stopAngle as Float)
+        shakeAnimation.autoreverses = true
+        shakeAnimation.duration = 0.15
+        shakeAnimation.repeatCount = 10000
+        shakeAnimation.timeOffset = 290 * drand48()
+        
+        let layer: CALayer = self.layer
+        layer.add(shakeAnimation, forKey:"shaking")
+    }
+    // Function to stop the shaking animation on a view.
+    
+    func stopShaking() {
+        let layer: CALayer = self.layer
+        layer.removeAnimation(forKey: "shaking")
+    }
 }
--- CrewBid iPad_Swift/Views/ViewControllers/HomeScreen/CBDocumentsCollectionViewController.swift
+++ CrewBid iPad_Swift/Views/ViewControllers/HomeScreen/CBDocumentsCollectionViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class CBDocumentsCollectionViewController: BaseViewController {
-
+    
     @IBOutlet weak var bidDownloadButton: UIButton!
     @IBOutlet weak var settingsButton: UIButton!
     @IBOutlet weak var lblHome: UILabel!
@@ -16,6 +16,10 @@ class CBDocumentsCollectionViewController: BaseViewController {
     @IBOutlet weak var helpMenuButton: UIButton!
     @IBOutlet weak var collectionView: UICollectionView!
     var bdPrd:Int!
+    
+    var isPlusImage = true
+    var selectedRows : [Int] = []
+    var collectionViewData = [1,2,3,4,5,6,7,8,9]
     
     let viewModel = DocumentsCollectionViewModel()
     
@@ -29,16 +33,30 @@ class CBDocumentsCollectionViewController: BaseViewController {
     }
     
     @IBAction func downloadBid(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "BidInfo", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "CBNewBidVC") as! CBNewBidVC
-        vc.preferredContentSize = CGSize(width: 600, height: 550)
-        self.present(vc, animated: true)
+        if isPlusImage {
+            let storyboard = UIStoryboard(name: "BidInfo", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "CBNewBidVC") as! CBNewBidVC
+            vc.preferredContentSize = CGSize(width: 600, height: 550)
+            self.present(vc, animated: true)
+        }
+        else {
+            guard !selectedRows.isEmpty else { return }
+            let sortedIndices = selectedRows.sorted(by: >)
+            for index in sortedIndices {
+                collectionViewData.remove(at: index)
+            }
+            let indexPaths = sortedIndices.map { IndexPath(item: $0, section: 0) }
+            collectionView.deleteItems(at: indexPaths)
+            selectedRows.removeAll()
+            //            MARK: wait
+            //            bidDownloadButton.setImage(UIImage(systemName: "trash"), for: .normal)
+        }
     }
     
     @IBAction func settingsAction(_ sender: Any) {
         let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "EmbeddedSettingsVC") as! EmbeddedSettingsVC
-//        vc.bidPeriod = nil
+        //        vc.bidPeriod = nil
         vc.bdPrd = bdPrd
         vc.preferredContentSize = CGSize(width: 300, height: 210)
         vc.modalPresentationStyle = .custom
@@ -47,15 +65,33 @@ class CBDocumentsCollectionViewController: BaseViewController {
     }
     
     @IBAction func editAction(_ sender: Any) {
-        print("Edit")
+        if self.editButton.currentTitle! == "Edit" {
+            selectedRows = []
+            self.editButton.setTitle("Done", for: .normal)
+            isPlusImage = false
+            let trashImage = UIImage(systemName: "trash.fill")
+            bidDownloadButton.setBackgroundImage(nil, for: .normal)
+            bidDownloadButton.setBackgroundImage(trashImage, for: .normal)
+            bidDownloadButton.tintColor = .white
+            bidDownloadButton.isEnabled = false
+            
+        } else {
+            self.editButton.setTitle("Edit", for: .normal)
+            isPlusImage = true
+            let plusImage = UIImage(named: "plus")
+            bidDownloadButton.setBackgroundImage(nil, for: .normal)
+            bidDownloadButton.setBackgroundImage(plusImage, for: .normal)
+            bidDownloadButton.isEnabled = true
+        }
+        self.collectionView.reloadData()
     }
     
     @IBAction func helpAction(_ sender: Any) {
         print("HelpMenu")
-//        print("HelpMenu")
+        //        print("HelpMenu")
         let storyBoard = UIStoryboard(name: "HelpMenu", bundle: nil)
         if let helpMenuVC = storyBoard.instantiateViewController(withIdentifier: "helpMenuViewController") as? helpMenuViewController{
-//            helpMenuVC.modalPresentationStyle = .formSheet
+            //            helpMenuVC.modalPresentationStyle = .formSheet
             helpMenuVC.preferredContentSize = CGSize(width: 764, height: 630)
             present(helpMenuVC, animated: true)
         }
@@ -66,7 +102,7 @@ class CBDocumentsCollectionViewController: BaseViewController {
 extension CBDocumentsCollectionViewController: UICollectionViewDataSource,UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 2
+        return collectionViewData.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -74,12 +110,51 @@ extension CBDocumentsCollectionViewController: UICollectionViewDataSource,UIColl
         cell.layer.cornerRadius = 10
         cell.layer.borderWidth = 8
         cell.layer.borderColor = CBColor.cbPurpleColor?.cgColor
+        collectionView.allowsMultipleSelection = true
+        
+        if self.editButton.currentTitle == "Edit" {
+            cell.stopWiggleAnimation()
+        } else {
+            cell.startWiggleAnimation()
+        }
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let storyboard = UIStoryboard(name: "BidDocument", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "CBBidDocumentController") as! CBBidDocumentController
-        self.navigationController?.pushViewController(vc, animated: true)
+        let cell: UICollectionViewCell = collectionView.cellForItem(at: indexPath)!
+        if self.editButton.currentTitle! == "Done" {
+            
+            cell.layer.borderColor = UIColor.systemOrange.cgColor
+            if selectedRows.contains(indexPath.item) {
+                selectedRows.remove(at: selectedRows.firstIndex(of: indexPath.item)!)
+            }else {
+                selectedRows.append(indexPath.item)
+            }
+            if selectedRows.count > 0 {
+                bidDownloadButton.isEnabled = true
+            }
+            else {
+                bidDownloadButton.isEnabled = false
+            }
+        }
+        else {
+            let storyboard = UIStoryboard(name: "BidDocument", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "CBBidDocumentController") as! CBBidDocumentController
+            self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        let cell: UICollectionViewCell = collectionView.cellForItem(at: indexPath)!
+        if self.editButton.currentTitle! == "Done" {
+            cell.layer.borderColor = CBColor.cbPurpleColor?.cgColor
+            if selectedRows.contains(indexPath.item) {
+                selectedRows.remove(at: selectedRows.firstIndex(of: indexPath.item)!)
+            }
+        }
+        if selectedRows.count > 0 {
+            bidDownloadButton.isEnabled = true
+        }else {
+            bidDownloadButton.isEnabled = false
+        }
     }
 }
