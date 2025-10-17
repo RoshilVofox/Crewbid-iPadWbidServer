7367d9c6df6d7795608c775968e0ee461f8dbd39
rishad <rishadp@vofoxsolutions.com>
2025-10-17 21:53:38 +0530

    preset keep local

---
+++ CrewBid iPad_Swift/Views/ViewControllers/Sync/CBCoreDataSync.swift
@@ -0,0 +1,12 @@
+//
+//  CBCoreDataSync.swift
+//  CrewBid iPad_Swift
+//
+//  Created by Rishad on 14/10/25.
+//
+
+import UIKit
+
+class CBCoreDataSync: NSObject {
+
+}
---
+++ CrewBid iPad_Swift/Views/ViewControllers/Sync/CBJSONSyncParsing.swift
@@ -0,0 +1,12 @@
+//
+//  CBJSONSyncParsing.swift
+//  CrewBid iPad_Swift
+//
+//  Created by Rishad on 14/10/25.
+//
+
+import UIKit
+
+class CBJSONSyncParsing: NSObject {
+
+}
